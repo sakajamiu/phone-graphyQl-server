fb86c3060673061745a839ac88c216dc0047a175
sakajamiu <sakaj20@gmail.com>
2022-03-10 19:03:26 +0100

    'user'

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const schema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        minlength: 3
+    }
+    friends:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Person'
+        }
+    ],
+})
+module.exports = new mongoose.model('User', schema)
